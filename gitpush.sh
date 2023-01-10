4613d3cf5e90fec260402c5ece12935a48eecd23
HASS Backup <homeassistant@gor1lla.de>
2023-01-10 17:05:16 +0100

    [2023-01-10] scheduled backup

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GIT_SSH_COMMAND="ssh -i /config/.ssh/id_ed25519"
+
+cd /config
+
+git add .
+
+TODAY=$(date +"%Y-%m-%d")
+
+git commit -m "[${TODAY}] scheduled backup"
+GIT_SSH_COMMAND="ssh -i /config/.ssh/id_ed25519" git push origin main
+exit
